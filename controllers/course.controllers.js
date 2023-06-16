426050899b053ea560f1c3d71f3182b9d4ee1447
zainmustafai <zforzain2000@gmail.com>
2023-06-16 18:23:55 +0500

    latest changes

--- controllers/course.controllers.js
+++ controllers/course.controllers.js
@@ -4,7 +4,7 @@ import Teacher from "../models/teacher.model.js";
 export const createNewCourse = async (req, res) => {
   try {
     // Extract the necessary data from the request body
-    const { courseCode, title, courseImage, syllabusFile } = req.body;
+    const { courseCode, title, } = req.body;
     // Get the ID of the teacher who created the course
     const teacherId = req.user.id; // Assuming the authenticated user's ID is stored in req.user.id
     // Create the course
@@ -20,7 +20,6 @@ export const createNewCourse = async (req, res) => {
     const teacher = await Teacher.findById(teacherId);
     teacher.courses.push(course._id);
     await teacher.save();
-
     res
       .status(201)
       .json({ success: true, message: "Course created successfully", course });
@@ -31,28 +30,20 @@ export const createNewCourse = async (req, res) => {
       .json({ success: false, message: "Failed to create course" });
   }
 };
-
-
-
 // Controller function to add a new teacher to the teachers array in the Course model
 const addTeacherToCourse = async (req, res) => {
   const { courseId, teacherId } = req.body;
-
   try {
     // Find the course by courseId
     const course = await Course.findById(courseId);
-
     // If the course is not found, return an error
     if (!course) {
       return res.status(404).json({ error: 'Course not found' });
     }
-
     // Add the teacherId to the teachers array in the course
     course.teachers.push(teacherId);
-
     // Save the updated course
     await course.save();
-
     // Return the updated course
     return res.json(course);
   } catch (error) {
