9df8e886e363fe4c5bd099e46d0fd7763b6f4f4c
zainmustafai <zforzain2000@gmail.com>
2023-06-23 12:29:36 -0700

    latest changes ooooffice

--- index.js
+++ index.js
@@ -34,13 +34,13 @@ app.use(
   })
 );
 app.use(cors());
-// Enable CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
+// // Enable CORS
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
